a3e829dde02034a88d44a17c935bf7e0785514e6
Ralph <blackgandalf83@gmail.com>
2023-01-17 16:20:40 -0500

    initial commit

---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+// Reexport your entry components here
+import SimpleFooter from '$lib/components/simple-footer/SimpleFooter.svelte';
+import ExternalLink from '$lib/components/external-link/ExternalLink.svelte'
+
+export { SimpleFooter, ExternalLink };
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import SimpleFooter from "../lib/components/simple-footer/SimpleFooter.svelte";
+</script>
+
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<SimpleFooter />
+
