858b40d6af0e70419ab44822cb2841ef0e8fea35
Ralph <blackgandalf83@gmail.com>
2023-01-31 16:24:13 -0500

    add ContactFooter

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,5 +2,6 @@
 import SimpleFooter from '$lib/components/simple-footer/SimpleFooter.svelte';
 import ExternalLink from '$lib/components/external-link/ExternalLink.svelte'
 import SimpleTabs from '$lib/components/simple-tabs/SimpleTabs.svelte'
+import ContactFooter from '$lib/components/contact-footer/ContactFooter.svelte';
 
-export { SimpleFooter, ExternalLink, SimpleTabs };
+export { SimpleFooter, ExternalLink, SimpleTabs, ContactFooter };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
 <script>
     import SimpleFooter from "../lib/components/simple-footer/SimpleFooter.svelte";
     import SimpleTabs from "../lib/components/simple-tabs/SimpleTabs.svelte";
+    import ContactFooter from "../lib/components/contact-footer/ContactFooter.svelte";
+    let address = `<p>343 S 2nd Ave</p>
+                    <p>Mount Vernon, NY 10550</p>            
+                    <a href="tel:9144194033">Tel: (914)419-4033</a>`
+
+    let hours = `<p>Mon. 	11:30am – 2:45pm & 5pm – 10pm</p>
+        <p>Tue. 	11:30am – 2:45pm & 5pm – 10pm</p>
+            <p>Wed. 	11:30am – 2:45pm & 5pm – 10pm</p>
+                <p>Thu. 	11:30am – 2:45pm & 5pm – 10pm</p>
+                    <p>Fri. 	11:30am – 2:45pm & 5pm – 10pm</p>
+                        <p>Sat. 	12pm – 10pm</p>
+                            <p>Sun. 	12pm – 10pm</p>
+
+                                <p>Holidays:   11:30am – 10pm</p>`
 </script>
 
 <h1 class="text-3xl">Welcome to your library project</h1>
@@ -14,4 +28,6 @@
 </div>
 
 <SimpleFooter />
+
+<ContactFooter address={address} hours={hours}/>
 
